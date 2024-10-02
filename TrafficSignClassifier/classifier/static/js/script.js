600700a262e0e98d9a5aa55704fa331652da2db8
Falze Labib <fazle.labib@gmail.com>
2024-10-02 21:08:37 +0600

    Remove unnecessary comments

--- TrafficSignClassifier/classifier/static/js/script.js
+++ TrafficSignClassifier/classifier/static/js/script.js
@@ -2,7 +2,6 @@ const uploadPanel = document.getElementById('upload-panel');
 const fileInput = document.getElementById('file-input');
 const clearButton = document.getElementById('clear-button');
 
-// Handle file selection and drag & drop
 fileInput.addEventListener('change', handleFileSelect);
 uploadPanel.addEventListener('dragover', (e) => {
     e.preventDefault();
@@ -16,32 +15,28 @@ uploadPanel.addEventListener('drop', (e) => {
     }
 });
 
-// Function to handle file selection
+
 function handleFileSelect() {
     const file = fileInput.files[0];
     if (file) {
         const reader = new FileReader();
         reader.onload = function (e) {
-            // Create an image element for the preview
+
             const previewImage = document.createElement('img');
             previewImage.src = e.target.result;
             previewImage.className = 'preview-image';
 
-            // Clear existing preview and add new one
             const uploadContent = uploadPanel.querySelector('.upload-content');
             if (uploadContent) {
                 uploadContent.remove();
             }
 
             uploadPanel.appendChild(previewImage);
-            // Hide the existing text and icon in the upload panel
         };
         reader.readAsDataURL(file);
     }
 }
 
-// Clear button functionality
 clearButton.addEventListener('click', (e) => {
-    // Show the existing text and icon in the upload panel again
     location.replace(location.href);
 });
