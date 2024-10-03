155d0fed9fe1bfc2e55899afc0384b1552d7d925
Falze Labib <fazle.labib@gmail.com>
2024-10-03 21:25:05 +0600

    Implement dark mode
    - Added toggle button to switch back and forth between light and dark mode
    - Updated styles to display dark/light mode colors
    - Added javascript function to trigger dark/light mode

--- TrafficSignClassifier/classifier/static/js/script.js
+++ TrafficSignClassifier/classifier/static/js/script.js
@@ -2,6 +2,33 @@ const uploadPanel = document.getElementById('upload-panel');
 const fileInput = document.getElementById('file-input');
 const clearButton = document.getElementById('clear-button');
 
+document.addEventListener("DOMContentLoaded", () => {
+    const toggle = document.getElementById("theme-toggle");
+    const darkModeIcon = document.getElementById("dark-mode-icon");
+    const lightModeIcon = document.getElementById("light-mode-icon");
+  
+    if (localStorage.getItem('theme') === 'dark') {
+      document.body.classList.add('dark-mode');
+      toggle.checked = true;
+      darkModeIcon.style.display = "block";
+      lightModeIcon.style.display = "none";
+    }
+  
+    toggle.addEventListener("change", function () {
+      if (this.checked) {
+        document.body.classList.add("dark-mode");
+        darkModeIcon.style.display = "block";
+        lightModeIcon.style.display = "none";
+        localStorage.setItem('theme', 'dark');
+      } else {
+        document.body.classList.remove("dark-mode");
+        darkModeIcon.style.display = "none";
+        lightModeIcon.style.display = "block";
+        localStorage.setItem('theme', 'light');
+      }
+    });
+  });  
+
 fileInput.addEventListener('change', handleFileSelect);
 uploadPanel.addEventListener('dragover', (e) => {
     e.preventDefault();
