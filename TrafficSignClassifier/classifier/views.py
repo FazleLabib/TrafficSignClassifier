c65395c5f3cf9a6bc21f3144886b1ba5a99a0cf7
Falze Labib <fazle.labib@gmail.com>
2024-10-04 19:52:32 +0600

    Add image pre-processing to classify function
    - Uploaded images will be pre-processed and converted to three channel images before being classified.

--- TrafficSignClassifier/classifier/views.py
+++ TrafficSignClassifier/classifier/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import ImageUploadForm
 from .utils import classify_image
+from PIL import Image
 from django.templatetags.static import static
 
 
@@ -10,15 +11,30 @@ def classify(request):
         if form.is_valid():
             image = form.cleaned_data['image']
 
-            label, confidence, class_val = classify_image(image)
+            img = Image.open(image)
+
+            if img.mode == 'RGBA':
+                img = img.convert('RGB')
+
+            
+            from io import BytesIO
+            img_io = BytesIO()
+            img.save(img_io, format='PNG')
+            img_io.seek(0)
+
+            label, confidence, class_val = classify_image(img_io)
+            
             meta_url = static('media/meta/' + str(class_val) + '.png') 
             confidence_percentage = float(confidence) * 100
-            if confidence_percentage.is_integer():
-                confidence_str = f"{int(confidence_percentage)}%"
-            else:
-                confidence_str = f"{confidence_percentage:.1f}%"
-            return render(request, 'home.html', {'form': form, 'label': label, 'confidence': confidence_str, 'meta_url': meta_url})
+            confidence_str = f"{int(confidence_percentage)}%" if confidence_percentage.is_integer() else f"{confidence_percentage:.1f}%"
+
+            return render(request, 'home.html', {
+                'form': form, 
+                'label': label, 
+                'confidence': confidence_str, 
+                'meta_url': meta_url
+            })
     else:
         form = ImageUploadForm()
-        
+
     return render(request, 'home.html', {'form': form})
